68b6e65acc10d98a6d27408eb0aaf88a388b9d93
Chrismessiah <chrabd@kth.se>
2016-02-10 16:07:10 +0100

    Modular header and footer

--- Lab2/js/app.js
+++ Lab2/js/app.js
@@ -1,9 +1,9 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
-	
+
 	//And create the needed controllers and views
-	var select = "#exampleView";
+	var select = "#root";
 	var view = new ExampleView($(select), model);
 
 });
--- Lab2/js/view/exampleView.js
+++ Lab2/js/view/exampleView.js
@@ -2,10 +2,14 @@
 var ExampleView = function (container, model) {
 	this.container = container;
 	this.model = model;
+
+	container.find("#page-head").append('<div class="row header"><h2>HOMELETTE</h2><h4>From the best chefs in the world directly into your kitchen!</h4></div><style type="text/css">body {margin: 0px;}.header, .footer {background-color: #191919;}.header > h2, .header > h4, .footer > h4 {position: relative;display: inline-block;left: 20px;font-family: "Gill Sans";color: white;}.header > h2 {margin-top: 10px;}.header > h4 {left: 40px;}</style>');
+	container.find("#page-foot").append('<div class="row footer"><h4>Made by Christian and Adelina</h2></div>');
+
 	// Get all the relevant elements of the view (ones that show data
-  	// and/or ones that responed to interaction)
+  // and/or ones that responed to interaction)
 	this.numberOfGuests = container.find("#numberOfGuests");
-	
+
 	// this.numberOfGuests.html(model.getNumberOfGuests()); // use updateGuests(); instead after function-declaration
 
 	this.plusButton = container.find("#plusGuest");
@@ -26,7 +30,7 @@ var ExampleView = function (container, model) {
 	var updateGuests = function () {
 		var num = model.getNumberOfGuests();
 		container.find("#numberOfGuests").html(num);
-	}
+	};
 
 	updateGuests();
 
@@ -41,11 +45,10 @@ var ExampleView = function (container, model) {
 		if (num > 1) {
 			model.setNumberOfGuests(num - 1);
 			updateGuests();
-		};
+		}
 	});
 
 	//  ******** Move to controller later!! *******
 
-	
-}
- 
+
+};
