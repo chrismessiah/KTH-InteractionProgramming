75bcd9bf9ce1ddfe116f021b627bf50256bcda55
Chrismessiah <chrabd@kth.se>
2016-02-10 20:38:06 +0100

    guestsColumn fully functional

--- Lab2/js/model/dinnerModel.js
+++ Lab2/js/model/dinnerModel.js
@@ -1,11 +1,11 @@
 //DinnerModel Object constructor
 var DinnerModel = function() {
- 
+
 	this.guestNum = 1;
 	this.chosenMeal = {
-		"starter" : "",
-		"mainDish" : "",
-		"dessert" : ""
+		"starter" : 1,
+		"mainDish" : 100,
+		"dessert" : 200
 	};
 	this.dinnerOptions = {
 		"veggie" : false,
@@ -13,21 +13,21 @@ var DinnerModel = function() {
 		"gluten" : false,
 		"laktos" : false,
 		"nuts" : false
-	}
+	};
 
 
 	this.setNumberOfGuests = function(num) {
 		this.guestNum = num;
-	}
+	};
 
 	this.getNumberOfGuests = function() {
 		return this.guestNum;
-	}
+	};
 
 	this.getSelectedDish = function(type) {
 		var dishes = this.getAllDishes(type);
 		return dishes;
-	}
+	};
 
 	this.getFullMenu = function() {
 		var starters = this.getAllDishes("starter");
@@ -39,17 +39,17 @@ var DinnerModel = function() {
 			"desserts" : desserts
 		};
 		return jsonMenu;
-	}
+	};
 
 	this.getAllIngredients = function() {
 		var allIngredients = [];
 		var menu = this.getFullMenu();
-		for (key in menu) {
+		for (var key in menu) {
 			var courseType = menu[key];
 			var courseType = [courseType[0], courseType[1], courseType[2]]
-			for (course in courseType) {
+			for (var course in courseType) {
 				var meal = courseType[course];
-				for (i in meal.ingredients) {
+				for (var i in meal.ingredients) {
 					var ingredient = meal.ingredients[i];
 					allIngredients.push(ingredient.name);
 				};
@@ -60,6 +60,15 @@ var DinnerModel = function() {
 
 	//Returns the total price of the menu (all the ingredients multiplied by number of guests).
 	this.getTotalMenuPrice = function() {
+	}
+
+	this.getTotalDishPrice = function(id) {
+		var total_price = 0
+		var dish = this.getDish(id);
+		for (var i in dish.ingredients) {
+		 	total_price += dish.ingredients[i].price;
+		}
+		return total_price;
 	}
 
 	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
@@ -95,24 +104,36 @@ var DinnerModel = function() {
 			}
 		}
 	  	return dish.type == type && found;
-	  });	
+	  });
 	}
 
 	//function that returns a dish of specific ID
 	this.getDish = function (id) {
-	  for(key in dishes){
-			if(dishes[key].id == id) {
-				return dishes[key];
+	  for(var key in dishes){
+			if (dishes.hasOwnProperty(key)) {
+				if(dishes[key].id == id) {
+					return dishes[key];
+				}
 			}
 		}
 	}
 
+	this.getChosenDishes = function () {
+		var list = [];
+		for(var key in this.chosenMeal){
+			if (this.chosenMeal.hasOwnProperty(key)) {
+				list.push(this.getDish(this.chosenMeal[key]));
+			}
+		}
+		return list;
+	}
 
-	// the dishes variable contains an array of all the 
+
+	// the dishes variable contains an array of all the
 	// dishes in the database. each dish has id, name, type,
 	// image (name of the image file), description and
-	// array of ingredients. Each ingredient has name, 
-	// quantity (a number), price (a number) and unit (string 
+	// array of ingredients. Each ingredient has name,
+	// quantity (a number), price (a number) and unit (string
 	// defining the unit i.e. "g", "slices", "ml". Unit
 	// can sometimes be empty like in the example of eggs where
 	// you just say "5 eggs" and not "5 pieces of eggs" or anything else.
@@ -122,7 +143,7 @@ var DinnerModel = function() {
 		'type':'starter',
 		'image':'toast.jpg',
 		'description':"In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'eggs',
 			'quantity':0.5,
 			'unit':'',
@@ -154,7 +175,7 @@ var DinnerModel = function() {
 		'type':'starter',
 		'image':'sourdough.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'active dry yeast',
 			'quantity':0.5,
 			'unit':'g',
@@ -176,7 +197,7 @@ var DinnerModel = function() {
 		'type':'starter',
 		'image':'bakedbrie.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'round Brie cheese',
 			'quantity':10,
 			'unit':'g',
@@ -198,7 +219,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'meatballs.jpg',
 		'description':"Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'extra lean ground beef',
 			'quantity':115,
 			'unit':'g',
@@ -260,7 +281,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'bakedbrie.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ingredient 1',
 			'quantity':1,
 			'unit':'pieces',
@@ -282,7 +303,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'meatballs.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ingredient 1',
 			'quantity':2,
 			'unit':'pieces',
@@ -304,7 +325,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'meatballs.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ingredient 1',
 			'quantity':1,
 			'unit':'pieces',
@@ -326,7 +347,7 @@ var DinnerModel = function() {
 		'type':'dessert',
 		'image':'icecream.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ice cream',
 			'quantity':100,
 			'unit':'ml',
@@ -338,7 +359,7 @@ var DinnerModel = function() {
 		'type':'dessert',
 		'image':'icecream.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ice cream',
 			'quantity':100,
 			'unit':'ml',
@@ -350,7 +371,7 @@ var DinnerModel = function() {
 		'type':'dessert',
 		'image':'icecream.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ice cream',
 			'quantity':100,
 			'unit':'ml',
--- Lab2/js/view/guestsColumn.js
+++ Lab2/js/view/guestsColumn.js
@@ -3,24 +3,39 @@ var guestsColumn = function (container, model) {
   this.model = model;
 
 
+
+
   // ********* Move to controller later!! ***********
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
-	
+	var updateMeals = function () {
+		this.meals = container.find("#meals");
+		var dishes = model.getChosenDishes();
+		var toAppend = "";
+		var total_cost = 0;
+		var dish_cost = 0;
+		for (var i in dishes) {
+				dish_cost = model.guestNum * model.getTotalDishPrice(dishes[i].id);
+				total_cost = total_cost + dish_cost;
+				toAppend = toAppend + '<div class="meal"><p class="dishes">'+dishes[i].name+'</p><p class="prices">'+dish_cost+'</p></div>';
+		}
+		container.find(".total-cost").html(total_cost);
+		this.meals.html(toAppend);
+	};
+
 	var updateGuests = function () {
 		var num = model.getNumberOfGuests();
 		container.find("#guestCount").html(num);
+		updateMeals()
 	};
 
 	updateGuests();
 
-	this.plusButton.click(function(){
+	container.find("#plusGuest").click(function(){
 		var num = model.getNumberOfGuests();
 		model.setNumberOfGuests(num + 1);
 		updateGuests();
 	});
 
-	this.minusButton.click(function(){
+	container.find("#minusGuest").click(function(){
 		var num = model.getNumberOfGuests();
 		if (num > 1) {
 			model.setNumberOfGuests(num - 1);
