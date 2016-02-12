368dde264e022e6c577b2f82da49f30e90d5ef26
Chrismessiah <chrabd@kth.se>
2016-02-12 17:43:05 +0100

    Fixade massa css

--- Lab2/js/view/selectDishView.js
+++ Lab2/js/view/selectDishView.js
@@ -8,20 +8,30 @@ var selectDishView = function (container, model) {
   var dish;
   toAppend = '';
   for (var i = 0; i < model.dishes.length; i++) {
-    dish = model.dishes[i]
+    dish = model.dishes[i];
     extraclass = 'a' + dish.id;
+
+    if (i === 0) {
+      toAppend = toAppend + '<div class="row">';
+    }
 
 
     toAppend = toAppend + '<div class="col-md-2 one-meal">';
     toAppend = toAppend + '<div class="meal-pic ' + extraclass + '"></div>';
-    toAppend = toAppend + '<div class="meal-name-box><div class="center">';
+    toAppend = toAppend + '<div class="meal-name-box"><div class="center">';
     toAppend = toAppend + '<p class="text-center black">' + dish.name + '</p>';
-    toAppend = toAppend + '</div>';
+    toAppend = toAppend + '</div></div>';
     toAppend = toAppend + '<p class="black">'+ dish.description.substring(0,40) +'</p></div>';
 
     toAppend = toAppend + '<style>';
     toAppend = toAppend + '.' + extraclass + '{background-image: url("./images/'+ dish.image +'");}';
     toAppend = toAppend + '</style>';
+    if (i === 5) {
+      toAppend = toAppend + '</div><div class="row">';
+    }
+    if (i === (model.dishes.length-1)) {
+      toAppend = toAppend + '</div>';
+    }
   }
 
 
