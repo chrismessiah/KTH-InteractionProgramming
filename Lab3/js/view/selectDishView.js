bc3ab096365aa63ba10521ef9ff6857ea64868a7
Chrismessiah <chrabd@kth.se>
2016-02-27 09:57:17 +0100

    searcher functional again

--- Lab3/js/view/selectDishView.js
+++ Lab3/js/view/selectDishView.js
@@ -8,11 +8,14 @@ var selectDishView = function (container, model) {
   this.mealContainers = container.find(".one-meal");
 
   this.update = function() {
-    var dish;
+    var dish, currentDishContainer;
     var toAppend = '';
-    for (var i = 0; i < model.showMeals.length; i++) {
-      if (i >= this.mealContainers.length) {
-        container.find(this.mealContainers[i]).html("");
+    for (var i = 0; i < this.mealContainers.length; i++) {
+      currentDishContainer = container.find(this.mealContainers[i]);
+      if (i >= model.showMeals.length) {
+        currentDishContainer.html("");
+        currentDishContainer.attr("id","");
+        currentDishContainer.addClass("one-meal-clickable");
       } else {
         dish = model.showMeals[i];
         extraclass = 'a' + dish.id;
@@ -25,9 +28,9 @@ var selectDishView = function (container, model) {
         toAppend = toAppend + '<style>';
         toAppend = toAppend + '.' + extraclass + '{background-image: url("./images/'+ dish.image +'");}';
         toAppend = toAppend + '</style>';
-        container.find(this.mealContainers[i]).html(toAppend);
-        container.find(this.mealContainers[i]).attr("id",dish.id);
-        container.find(this.mealContainers[i]).addClass("one-meal-clickable");
+        currentDishContainer.html(toAppend);
+        currentDishContainer.attr("id",dish.id);
+        currentDishContainer.addClass("one-meal-clickable");
         toAppend = '';
       }
     }
