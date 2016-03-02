c35560fb06e2f065bafbe442fe6aa46ee6477209
Chrismessiah <chrabd@kth.se>
2016-03-02 15:02:08 +0100

    css fix

--- Lab4/js/view/selectDishView.js
+++ Lab4/js/view/selectDishView.js
@@ -13,11 +13,9 @@ var selectDishView = function (container, model) {
     var counter = 0;
      for (var i = 0; i < model.showMeals.length; i++) {
        dish = model.showMeals[i];
-       console.log(dish);
        extraclass = 'a' + dish["RecipeID"];
 
        if (counter === 0) {toAppend = toAppend + '<div class="row">';}
-
        toAppend = toAppend + '<div class="col-md-2 one-meal" id="' + dish["RecipeID"] + '">';
        toAppend = toAppend + '<div class="meal-pic ' + extraclass + '"></div>';
        toAppend = toAppend + '<div class="meal-name-box"><div class="center">';
@@ -25,7 +23,7 @@ var selectDishView = function (container, model) {
        toAppend = toAppend + '</div></div>';
        toAppend = toAppend + '<p class="black">'+ /*dish["Description"].substring(0,40)*/ +'...</p></div>';
        toAppend = toAppend + '<style>';
-       toAppend = toAppend + '.' + extraclass + '{background-image: url("'+ dish["WebURL"] +'");}';
+       toAppend = toAppend + '.' + extraclass + '{background-image: url("'+ dish["HeroPhotoUrl"] +'");}';
        toAppend = toAppend + '</style></div>';
 
        counter += 1;
